54660c97243b9fa9cdbdf50521608631f09bb1fa
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-25 15:03:57 +0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Naveed on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
